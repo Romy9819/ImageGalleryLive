2f58204fa53c38cf7dae99faea2d01147b6be84b
Romy9819 <romymathew732@gmail.com>
2023-08-07 18:07:59 +0530

    final

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const userRoute = require('./routes/users');
 const imageRoute = require('./routes/images');
-// const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,16 +12,22 @@ mongoose.connect('mongodb+srv://romymathew732:Romy12345@cluster0.i9rqt1e.mongodb
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log('MongoDB connected');
+    // console.log('MongoDB connected');
   })
   .catch((error) => {
     console.error('MongoDB connection error:', error);
   });
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 app.use(userRoute, imageRoute)
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function(req, res){
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //Starting the server
 const port = 5000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost/${port}`)
+    // console.log(`Server is running on http://localhost/${port}`)
 })
